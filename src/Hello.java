dcc4aba2c8a4025d3fa514f5f91f98a038e4460c
wangbin123634 <418180062@qq.com>
2016-11-29 14:26:39 +0800

    添加第一个项目

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+
+/**
+ * @Project: first-git-project
+ * @Description TODO
+ * @author 王斌
+ * @date 2016年11月29日
+ * @version V1.0
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
